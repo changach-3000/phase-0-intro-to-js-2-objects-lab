83c56110cd3f50bb6c7e4c0d7bf08be4fedcaf2b
changach-3000 <sharon.changach@student.moringaschool.com>
2023-03-17 07:57:00 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {};
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    const update2 = {... employee};
+    update2[key] = value;
+    return update2;
+    
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newemployee = {...employee};
+    delete newemployee[key];
+    return newemployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
+
+
